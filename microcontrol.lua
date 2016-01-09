1c8ba36901310a82d9452d5a16eb0ee7082945d3
Kyrylo Silin <silin@kyrylo.org>
2016-01-09 15:48:36 +0200

    Make the map playable

--- microcontrol.lua
+++ microcontrol.lua
@@ -1,26 +1,4 @@
-map_vsn = 1
-
-Msg = function(msg)
-  Media.DisplayMessage(msg, 'Briefing')
-end
-
--- ParadropRankCrates = function()
---   Utils.Do(centreLocations, function(actor)
---     local powerproxy = Actor.Create('powerproxy.paratroopers', true, { Owner = neutral })
---     powerproxy.SendParatroopers(actor, false, Facing.South)
---   end)
-
---   -- local paradropLocation = Utils.Random(centreLocations)
---   -- local powerproxy = Actor.Create('powerproxy.paratroopers', true, { Owner = neutral })
---   -- powerproxy.SendParatroopers(paradropLocation, false, Facing.South)
-
---   -- transport.Paradrop(paradropLocation)
-
---   -- for i, location in ipairs(centreLocations) do
---   --   Paradrop.Paradrop(location)
---   --   Actor.Create('crate', true, { Owner = neutral, Location = location } )
---   -- end
--- end
+local map_vsn = 1
 
 function TableSize(T)
   local count = 0
@@ -34,59 +12,151 @@ end
 
 function InitWaves()
   return {
-    -- Round 1
+    -- Round 1 (Warm up, simple units)
     {
       { 'e1', 'e1', 'e1', 'e1', 'e1' },
-      {},
       { 'e1', 'e1', 'e1', 'e1', 'e1' },
-      {},
-      {},
-      {}
-    },
-    -- Round 2
-    {
-      { 'medi' },
       { 'e1', 'e1', 'e1', 'e1', 'e1' },
-      { 'medi' },
-      { 'e1', 'e1', 'e1', 'e1', 'e1' },
-      {},
-      { 'e1', 'e1', 'e1', 'e1', 'e1' }
-    },
-    -- Round 3
-    {
-      { 'medi', 'dog' },
-      { 'e1', 'e1', 'e1', 'e1', 'e1' },
-      { 'medi', 'dog' },
       { 'e1', 'e1', 'e1', 'e1', 'e1' },
       { 'e1', 'e1', 'e1', 'e1', 'e1' },
       { 'e1', 'e1', 'e1', 'e1', 'e1' }
     },
-    -- Round 4
+    -- Round 2 (Early Allies infantry)
     {
-      {},
-      { 'e3', 'e3', 'e3' },
-      {},
-      { 'ftrk' },
-      { 'e1', 'e1', 'e1', 'e1', 'e1' },
-      { 'ftrk' }
+      { 'medi', 'e1', 'e1', 'e1', 'e1' },
+      { 'medi', 'e1', 'e1', 'e1', 'e1' },
+      { 'e1',   'e1', 'e1', 'e1', 'e1' },
+      { 'medi', 'e1', 'e1', 'e1', 'e1' },
+      { 'medi', 'e1', 'e1', 'e1', 'e1' },
+      { 'medi', 'e1', 'e1', 'e1', 'e1' }
+    },
+    -- Round 3 (Early Soviet infantry)
+    {
+      { 'dog', 'e1', 'e1', 'e1', 'e2' },
+      { 'dog', 'e1', 'e1', 'e1', 'e2' },
+      { 'e1',  'e1', 'e1', 'e1', 'e2' },
+      { 'dog', 'e1', 'e1', 'e1', 'e2' },
+      { 'dog', 'e1', 'e1', 'e1', 'e2' },
+      { 'dog', 'e1', 'e1', 'e1', 'e2' }
+    },
+    -- Round 4 (Light Allies army)
+    {
+      { '1tnk', 'e1', 'e1', 'e1', 'e3' },
+      { 'jeep', 'e3' },
+      { '1tnk', 'e1', 'e1', 'e1', 'e3' },
+      { '1tnk', 'e1', 'e1', 'e1', 'e3' },
+      { 'jeep', 'e3' },
+      { '1tnk', 'e1', 'e1', 'e1', 'e3' }
+    },
+    -- Round 5 (Light Soviet army)
+    {
+      { 'ftrk', 'e4', 'e4', 'e3', 'e3' },
+      { 'apc',  'e2', 'e2', 'e3', 'e3' },
+      { 'e1',   'e1', 'e1', 'e1', 'e1' },
+      { 'apc',  'e2', 'e2', 'e3', 'e3' },
+      { 'ftrk', 'e4', 'e4', 'e3', 'e3' },
+      { 'e1',   'e1', 'e1', 'e1', 'e1' }
+    },
+    -- Round 6 (Mid-game Allies tank army)
+    {
+      { '2tnk', '2tnk', 'e3', 'mech', 'e3' },
+      { '2tnk', '2tnk', 'e1', 'e3',   'e1' },
+      { '1tnk', '1tnk', 'e1', 'mech', 'e1' },
+      { '1tnk', '1tnk', 'e1', 'mech', 'e1' },
+      { '2tnk', '2tnk', 'e1', 'mech', 'e1' },
+      { '2tnk', '2tnk', 'e3', 'e3',   'e3' }
+    },
+    -- Round 7 (Mid-game Soviet tank army)
+    {
+      { '3tnk', '3tnk', '3tnk', 'e3', 'e3' },
+      { '3tnk', '3tnk', '3tnk', 'e3', 'e3' },
+      { '3tnk', 'e1',   'e1',   'e1', 'e1' },
+      { '3tnk', 'e1',   'e1',   'e1', 'e1' },
+      { '3tnk', '3tnk', '3tnk', 'e3', 'e3' },
+      { '3tnk', '3tnk', '3tnk', 'e3', 'e3' },
+    },
+    -- Round 8 (Mid-game Allies long range army)
+    {
+      { '2tnk', '1tnk', 'jeep', 'e3', 'e3' },
+      { '2tnk', 'arty', 'mech', 'e1', 'e1' },
+      { '2tnk', '1tnk', 'jeep', 'e1', 'e1' },
+      { '2tnk', 'arty', 'mech', 'e1', 'e1' },
+      { '2tnk', 'e1',   'e1',   'e1', 'e1' },
+      { '2tnk', 'e3',   'e3',   'e3', 'e3' }
+    },
+    -- Round 9 (Mid-game Soviet long range army)
+    {
+      { '3tnk', 'v2rl', 'e3',   'e3', 'e3' },
+      { '3tnk', 'v2rl', 'e4',   'e4', 'e4' },
+      { '3tnk', 'v2rl', 'e2',   'e2', 'e2' },
+      { '3tnk', 'v2rl', 'e1',   'e1', 'e1' },
+      { '3tnk', 'apc',  'ftrk', 'e1', 'e1' },
+      { '3tnk', 'apc',  'ftrk', 'e1', 'e1' }
+    },
+    -- Round 10 (High tech English army)
+    {
+      { '2tnk', 'mech', 'arty', 'e3', 'e3' },
+      { '2tnk', 'mech', 'arty', 'e3', 'e3' },
+      { '2tnk', 'mech', 'arty', 'e3', 'e3' },
+      { '2tnk', 'mech', 'arty', 'e1', 'e1' },
+      { '2tnk', 'jeep', 'stnk', 'e1', 'e1' },
+      { '2tnk', 'jeep', 'stnk', 'e1', 'e1' }
+    },
+    -- Round 11 (High tech Russian army)
+    {
+      { '3tnk', 'ttnk', 'v2rl', 'shok', 'e1' },
+      { '3tnk', 'ttnk', 'v2rl', 'shok', 'e1' },
+      { '4tnk', 'ttnk', 'v2rl', 'shok', 'e1' },
+      { '4tnk', 'ttnk', 'shok', 'shok', 'e1' },
+      { '3tnk', 'ttnk', 'shok', 'shok', 'e1' },
+      { '3tnk', 'ttnk', 'shok', 'e1',   'e1' }
+    },
+    -- Round 12 (High tech German army)
+    {
+      { 'ctnk', 'mech', 'arty', 'jeep', 'e1' },
+      { 'ctnk', 'mech', '2tnk', 'e3',   'e1' },
+      { 'ctnk', 'mech', '2tnk', 'e3',   'e1' },
+      { 'ctnk', 'mech', '2tnk', 'e3',   'e1' },
+      { 'ctnk', 'mech', '2tnk', 'e3',   'e1' },
+      { 'ctnk', 'mech', 'arty', 'e3',   'e1' }
+    },
+    -- Round 13 (High tech Ukrainian army)
+    {
+      { 'dtrk', '3tnk', 'v2rl', 'e3', 'e1' },
+      { '4tnk', '3tnk', 'v2rl', 'e3', 'e1' },
+      { '4tnk', '3tnk', 'v2rl', 'e3', 'e1' },
+      { '4tnk', '3tnk', 'v2rl', 'e3', 'e1' },
+      { '4tnk', '3tnk', 'v2rl', 'e3', 'e1' },
+      { '4tnk', '3tnk', 'e4',   'e4', 'e4' }
+    },
+    -- Round 14 (High tech French army)
+    {
+      { '2tnk', 'arty', 'mgg',  'e3', 'e3' },
+      { '2tnk', 'arty', 'mgg',  'e3', 'e1' },
+      { '2tnk', 'arty', 'mgg',  'e1', 'e1' },
+      { '2tnk', 'arty', 'mgg',  'e1', 'e1' },
+      { '2tnk', 'arty', 'jeep', 'e3', 'e1' },
+      { '2tnk', 'arty', 'jeep', 'e3', 'e3' }
+    },
+    -- Round 15 (Mixed army, no infantry)
+    {
+      { 'dtrk', 'arty', 'arty', 'mgg', 'mgg' },
+      { '4tnk', '4tnk', 'v2rl', 'v2rl', 'jeep' },
+      { 'jeep', 'ftrk', 'ftrk', '2tnk', '2tnk' },
+      { '3tnk', '3tnk', '3tnk', '3tnk', '3tnk' },
+      { 'arty', 'arty', 'apc', 'apc', '1tnk'   },
+      { '1tnk', '1tnk', 'mnly.ap', 'mnly.ap', 'mnly.ap' }
     }
-    -- Round 5
   }
 end
 
 function ShowScoreboard(players)
-  local msg = '[scoreboard] '
+  local scores = ''
   Utils.Do(players, function(player)
-    msg = msg .. player.Player.Name .. ' - ' .. player.Points .. '. '
+    scores = scores .. player.Player.Name .. ' - ' .. player.Points .. '. '
   end)
-  Msg(msg)
-end
 
-function BeginGame(players, waves)
-  local totalRounds = TableSize(waves)
-  local currentRound = 1
-
-  BeginRound(currentRound, waves, players, totalRounds)
+  Media.DisplayMessage(scores, 'Scoreboard')
 end
 
 function IsHusk(actor)
@@ -97,10 +167,11 @@ function EndsWith(str, tail)
   return #str >= #tail and string.sub(str, 1 + #str - #tail) == tail
 end
 
-function EndRound()
-  local unitsToDestroy = Map.ActorsInBox(Map.TopLeft,Map.BottomRight, function(actor)
-    return IsHusk(actor) or actor.Owner ~= neutral or actor.HasProperty('Crate')
-  end)
+function ClearMap()
+  local unitsToDestroy =
+    Map.ActorsInBox(Map.TopLeft,Map.BottomRight, function(actor)
+      return IsHusk(actor) or actor.Owner ~= neutral or actor.HasProperty('Crate')
+    end)
 
   Utils.Do(unitsToDestroy, function(actor)
     if not actor.IsDead then
@@ -115,9 +186,29 @@ function EndRound()
   end)
 end
 
+function EndRound(game, players, waves)
+  ClearMap()
+  ShowScoreboard(players)
+  Utils.Do(players, function(player)
+    player.RoundDeathCounter = 0
+    player.RoundArmy = {}
+  end)
+
+  game.CurrentRound = game.CurrentRound + 1
+
+  if game.CurrentRound > game.TotalRounds then
+    EndGame(players)
+    return
+  end
+
+  Trigger.AfterDelay(DateTime.Seconds(5), function()
+    BeginRound(game, players, waves)
+  end)
+end
+
 function EndGame(players)
   local bestScore = 0
-  local winner
+  local winner = players[1]
 
   Utils.Do(players, function(player)
     if player.Points > bestScore then
@@ -126,112 +217,94 @@ function EndGame(players)
     end
   end)
 
-  Msg('Player ' .. winner.Player.Name .. ' won the game!')
+  Media.DisplayMessage(
+    winner.Player.Name .. ' earned ' .. bestScore .. ' points and became the new Micro Control champion!',
+    'Game'
+  )
 
-  Utils.Do(players, function(player)
-    if player == winner then
-      winner.Player.MarkCompletedObjective(winner.Player.AddPrimaryObjective('win'))
-    else
-      player.Player.MarkFailedObjective(player.Player.AddPrimaryObjective('lose'))
-    end
+  Trigger.AfterDelay(DateTime.Seconds(2), function()
+    Utils.Do(players, function(player)
+      if player == winner then
+        winner.Player.MarkCompletedObjective(winner.Player.AddPrimaryObjective('win'))
+      else
+        player.Player.MarkFailedObjective(player.Player.AddPrimaryObjective('lose'))
+      end
+    end)
   end)
 end
 
-function BeginRound(currentRound, waves, players, totalRounds)
-  if currentRound > totalRounds then
-    EndGame(players)
-    return
+function SpawnUnits(player, wave)
+  local initTable
+  local units = {}
+
+  for i, squad in ipairs(wave) do
+    for j, unit in ipairs(squad) do
+      initTable = {
+        Owner = player.Player,
+        Location = player.Waypoints[i].Location
+      }
+
+      table.insert(player.RoundArmy, Actor.Create(unit, true, initTable))
+    end
   end
 
-  Msg('Round ' .. currentRound)
+  return units
+end
 
+function SetTriggers(game, player, players, waves)
+  Utils.Do(player.RoundArmy, function(actor)
+    Trigger.OnKilled(actor, function()
+      player.RoundDeathCounter = player.RoundDeathCounter + 1
+
+      -- Media.DisplayMessage(player.Player.Name .. '..' .. player.RoundDeathCounter .. '/' .. TableSize(player.RoundArmy), 'DEBUG')
+
+      if player.RoundDeathCounter == TableSize(player.RoundArmy) then
+        game.RoundWinners[player] = nil
+        Media.DisplayMessage(player.Player.Name .. ' is no more.', 'Game')
+      end
+
+      if TableSize(game.RoundWinners) == 1 then
+        local winner = next(game.RoundWinners)
+        Media.DisplayMessage(winner.Player.Name .. ' won the round!', 'Game')
+        winner.Points = winner.Points + 1
+        EndRound(game, players, waves)
+      end
+    end)
+  end)
+end
+
+function BeginRound(game, players, waves)
+  Media.DisplayMessage(
+    'Starting round ' .. game.CurrentRound .. ' out of ' .. game.TotalRounds .. '!',
+    'Game'
+  )
   Utils.Do(players, function(player)
     Media.PlaySpeechNotification(player.Player, 'ReinforcementsArrived')
   end)
 
-  local wave = waves[currentRound]
-  local winCandidates = {}
+  local player
 
   for i=1,4 do
-    local player = players[i]
+    player = players[i]
 
     if player then
-      winCandidates[i] = i
-      local armySize = 0
-      local armyArrivedSize = 0
-
-
-      player.RoundArmy = {}
-
-      for j, squad in ipairs(wave) do
-        armySize = armySize + TableSize(squad)
-
-        player.RoundArmy[j] =
-          Reinforcements.Reinforce(neutral, squad, { player.Spawnpoint,
-                                   player.Waypoints[j].Location }, 25, function(actor)
-                                     armyArrivedSize = armyArrivedSize + 1
-                                     if armyArrivedSize == armySize then
-                                       Utils.Do(player.RoundArmy, function(sq)
-                                         Utils.Do(sq, function(a)
-                                           a.Owner = player.Player
-                                         end)
-                                       end)
-
-                                       Media.PlaySpeechNotification(player.Player, 'TimerStarted')
-                                     end
-                                   end)
-
-        Utils.Do(player.RoundArmy[j], function(actor)
-          Trigger.OnKilled(actor, function()
-            armySize = armySize - 1
-
-            if armySize == 0 then
-              winCandidates[i] = nil
-
-              if TableSize(winCandidates) == 1 then
-                for _, winner in pairs(winCandidates) do
-                  local winnerPlayer = players[winner]
-                  Msg('Player ' .. winnerPlayer.Player.Name .. ' won the round!')
-                  EndRound()
-                  winnerPlayer.Points = winnerPlayer.Points + 1
-                  ShowScoreboard(players)
-
-                  Trigger.AfterDelay(DateTime.Seconds(2), function()
-                    BeginRound(currentRound + 1, waves, players, totalRounds)
-                  end)
-                end
-              end
-            end
-          end)
-        end)
-      end
+      game.RoundWinners[player] = player
+      SpawnUnits(player, waves[game.CurrentRound])
+      SetTriggers(game, player, players, waves)
     end
   end
-
-  -- ParadropRankCrates()
 end
 
 function InitPlayers()
   local waypoints = {
-    -- Player 1
-    { AWaypoint1, AWaypoint2, AWaypoint3, AWaypoint4, AWaypoint5, AWaypoint6 },
-     -- Player 2
-    { BWaypoint1, BWaypoint2, BWaypoint3, BWaypoint4, BWaypoint5, BWaypoint6 },
-    -- Player 3
-    { CWaypoint1, CWaypoint2, CWaypoint3, CWaypoint4, CWaypoint5, CWaypoint6 },
-    -- Player 4
-    { DWaypoint1, DWaypoint2, DWaypoint3, DWaypoint4, DWaypoint5, DWaypoint6 }
-  }
-
-  local spawnpoints = {
     -- Player 1 (TopLeft)
-    CPos.New(1, 1),
-    -- Player 2 (TopRight)
-    CPos.New(1, 64),
+    { AWaypoint1, AWaypoint2, AWaypoint3, AWaypoint4, AWaypoint5, AWaypoint6 },
+     -- Player 2 (TopRight)
+    { BWaypoint1, BWaypoint2, BWaypoint3, BWaypoint4, BWaypoint5, BWaypoint6 },
     -- Player 3 (BottomRight)
-    CPos.New(64, 64),
+    { CWaypoint1, CWaypoint2, CWaypoint3, CWaypoint4, CWaypoint5, CWaypoint6 },
     -- Player 4 (BottomLeft)
-    CPos.New(64, 1)
+    { DWaypoint1, DWaypoint2, DWaypoint3, DWaypoint4, DWaypoint5, DWaypoint6 }
   }
 
   -- Neutral player owns crates and such
@@ -246,8 +319,9 @@ function InitPlayers()
       players[i] = {
         Player = player,
         Waypoints = waypoints[i],
-        Spawnpoint = spawnpoints[i],
-        Points = 0
+        Points = 0,
+        RoundArmy = {},
+        RoundDeathCounter = 0
       }
     end
   end
@@ -255,15 +329,33 @@ function InitPlayers()
   return players
 end
 
-function WorldLoaded()
-  Msg('Welcome to Micro Control v' .. map_vsn .. '!')
-  Msg('Your objective is to outmicro other players given a group of units.')
-  Msg('Each player has the same set of units as you.')
-
-  local players = InitPlayers()
+function BeginGame(game, players)
   local waves = InitWaves()
 
-  Trigger.AfterDelay(DateTime.Seconds(2), function()
-    BeginGame(players, waves)
+  game.TotalRounds = TableSize(waves)
+  game.CurrentRound = 1
+
+  BeginRound(game, players, waves)
+end
+
+function InitGame(players)
+  return {
+    RoundWinners = {},
+    CurrentRound = 0,
+    TotalRounds = 0
+  }
+end
+
+function WorldLoaded()
+  local game = InitGame()
+  local players = InitPlayers()
+
+  Media.DisplayMessage('Welcome to Micro Control v' .. map_vsn .. '!', 'Briefing')
+  Media.DisplayMessage('Your objective is to outmicro other players given a group of units.', 'Briefing')
+  Media.DisplayMessage('Each player has the same set of units as you.', 'Briefing')
+  Media.DisplayMessage('Good luck & have fun!', 'Briefing')
+
+  Trigger.AfterDelay(DateTime.Seconds(5), function()
+    BeginGame(game, players)
   end)
 end
