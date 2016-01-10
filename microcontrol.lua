372518965a4df255048ebc0421dac6fe8258bf74
Kyrylo Silin <silin@kyrylo.org>
2016-01-10 12:01:59 +0200

    microcontrol: better wording

--- microcontrol.lua
+++ microcontrol.lua
@@ -8,7 +8,7 @@ function WorldLoaded()
   Media.DisplayMessage('Map made by Kyrylo Silin', 'Briefing')
   Media.DisplayMessage('Your objective is to outmicro other players given a group of units.', 'Briefing')
   Media.DisplayMessage('Each player has the same set of units as you.', 'Briefing')
-  Media.DisplayMessage('Search for crates.' , 'Briefing')
+  Media.DisplayMessage('Pro tip: search for crates.' , 'Briefing')
   Media.DisplayMessage('Your cash will be used to determine the winner if the scores are equal.', 'Briefing')
   Media.DisplayMessage('Good luck & have fun!', 'Briefing')
 
